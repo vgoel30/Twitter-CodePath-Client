389c341df466bb4bf27a246fab8bd632e4d05e53
Varun Goel <babyvarun96@gmail.com>
2016-02-26 22:04:00 -0500

    Implemented pull to refresh

---
+++ TweetCell.swift
@@ -0,0 +1,24 @@
+//
+//  TweetCell.swift
+//  Twitter
+//
+//  Created by Varun Goel on 2/26/16.
+//  Copyright © 2016 Varun Goel. All rights reserved.
+//
+
+import UIKit
+
+class TweetCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- TweetsViewController.swift
+++ TweetsViewController.swift
@@ -8,23 +8,77 @@
 
 import UIKit
 
-class TweetsViewController: UIViewController {
-
+class TweetsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+  
+ 
+    
+    @IBOutlet var tweetTable: UITableView!
+   
+   
     //the array of tweets
-    var tweets:[Tweet]!
+    var tweets:[Tweet]?
+  
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Initialize a UIRefreshControl
+        let refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: "refreshControlAction:", forControlEvents: UIControlEvents.ValueChanged)
+        tweetTable.insertSubview(refreshControl, atIndex: 0)
+        
+    
+        tweetTable.dataSource = self
+        tweetTable.delegate = self
+        tweetTable.rowHeight = UITableViewAutomaticDimension
+        tweetTable.estimatedRowHeight = 120
+        
+        
         TwitterClient.sharedInstance.homeTimeline({ (tweets:[Tweet]) -> () in
-            self.tweets = tweets
-            for tweet in tweets{
-                print(tweet.text)
-            }
+            self.tweets = tweets as [Tweet]!
+            self.tweetTable.reloadData()
+          
+            
+
             }, failure:  { (error: NSError) -> () in
                 print(error.localizedDescription)
         })
-
+        
         // Do any additional setup after loading the view.
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if self.tweets != nil{
+            print(tweets!.count)
+            return tweets!.count
+        }else{
+            return 0
+        }
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tweetTable.dequeueReusableCellWithIdentifier("TweetCell", forIndexPath: indexPath) as! TweetCell
+        cell.selectionStyle = .None
+        //cell.tweet = tweets![indexPath.row]
+        
+        return cell
+    }
+    
+    // Makes a network request to get updated data
+    // Updates the tableView with the new data
+    // Hides the RefreshControl
+    func refreshControlAction(refreshControl: UIRefreshControl) {
+        
+        TwitterClient.sharedInstance.homeTimeline({ (tweets:[Tweet]) -> () in
+            self.tweets = tweets as [Tweet]!
+            self.tweetTable.reloadData()
+            
+            refreshControl.endRefreshing()
+            
+            
+            }, failure:  { (error: NSError) -> () in
+                print(error.localizedDescription)
+        })
     }
 
     override func didReceiveMemoryWarning() {
@@ -32,8 +86,11 @@ class TweetsViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
+    //function to check if internet connection is active or not
+    
+    
+    
+        /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
